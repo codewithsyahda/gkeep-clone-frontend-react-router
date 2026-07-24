8f5aed49143576962b22a84056cd55258898929a
Syahda Romansyah <codewithsyahda@gmail.com>
2026-07-24 22:48:40 +0700

    ci: merge Playwright reports from multiple shards

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: isCI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: isCI ? 'dot' : 'list',
+  reporter: isCI ? 'blob' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
