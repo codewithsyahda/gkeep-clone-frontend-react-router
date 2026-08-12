859d675a18baa27c8ff5adc5804941b1886c20f8
Syahda Romansyah <codewithsyahda@gmail.com>
2026-08-12 11:02:48 +0700

    chore: ensure MSW only runs in dev mode

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,7 +5,7 @@ import { HydratedRouter } from 'react-router/dom';
 import envConfig from './configs/envs';
 
 async function enableMsw() {
-  if (envConfig.dev.mock.msw) {
+  if (import.meta.env.DEV && envConfig.dev.mock.msw) {
     const { mswBrowserWorker } = await import('./tests/mocks/apis/mswBrowser');
     await mswBrowserWorker.start();
   }
