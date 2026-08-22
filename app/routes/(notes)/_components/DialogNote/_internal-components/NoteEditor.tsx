94508b7052916dad06bfdd4f56c10c8cbeddc596
Syahda Romansyah <codewithsyahda@gmail.com>
2026-08-22 10:24:43 +0700

    Revert "refactor: remove the unused dialog note title input auto-focus"
    This reverts commit 7b57969520b58cc3f28d7fa6ed7d59be8949f3a0.

--- app/routes/(notes)/_components/DialogNote/_internal-components/NoteEditor.tsx
+++ app/routes/(notes)/_components/DialogNote/_internal-components/NoteEditor.tsx
@@ -130,6 +130,24 @@ export default function NoteEditor({
     };
   }, [noteEditor.commands]);
 
+  useEffect(() => {
+    const inputTitleElem = inputTitleElemRef.current;
+
+    if (!inputTitleElem || disabledInputTitle) return;
+
+    inputTitleElem.focus();
+
+    const range = document.createRange();
+
+    range.selectNodeContents(inputTitleElem);
+    range.collapse(false);
+
+    const selection = window.getSelection();
+
+    selection?.removeAllRanges();
+    selection?.addRange(range);
+  }, [disabledInputTitle]);
+
   return (
     <Box
       data-component="inputs-group-container"
