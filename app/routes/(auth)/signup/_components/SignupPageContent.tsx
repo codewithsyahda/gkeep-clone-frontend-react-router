ca99acbfd4fd0ba47a1bab69ab02e6f6236f2200
Syahda Romansyah <codewithsyahda@gmail.com>
2026-08-24 09:43:10 +0700

    feat: update the sign-up page components

--- app/routes/(auth)/signup/_components/SignupPageContent.tsx
+++ app/routes/(auth)/signup/_components/SignupPageContent.tsx
@@ -1,5 +1,4 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useEffect } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router';
 import { z } from 'zod';
@@ -21,12 +20,6 @@ import AuthPageContainer from '../../_components/AuthPageContainer';
 
 import { SignupFormScheme } from '~/lib/definitions';
 import emitSnackbarAlert from '~/routes/_helpers/snackbarAlert';
-
-const useSession = (
-  await (import.meta.env.DEV
-    ? import('~/hooks/react-query/auth/__mocks__/useSession')
-    : import('~/hooks/react-query/auth/useSession'))
-).default;
 
 const useSignupWithEmail = (
   await (import.meta.env.DEV
@@ -62,38 +55,12 @@ export default function SignupPageContent() {
         alertText: 'Signing up is successful',
       });
     } catch (error) {
-      const errorMessage = (error as Error).message;
-
       emitSnackbarAlert({
-        alertText: errorMessage,
+        alertText: (error as Error).message,
         alertSeverity: 'error',
       });
     }
   };
-
-  const session = useSession();
-
-  const sessionData = session.data?.session;
-
-  useEffect(() => {
-    if (!session.isPending && sessionData) {
-      const redirect = async () => {
-        const firstName = sessionData.name.split(' ')[0];
-
-        await navigate('/', { replace: true });
-
-        emitSnackbarAlert({
-          alertText: `Welcome back, ${firstName}!`,
-        });
-      };
-
-      void redirect();
-    }
-  }, [navigate, session.isPending, sessionData]);
-
-  if (!mutSignupWithEmail.isSuccess && (session.isPending || sessionData)) {
-    return null;
-  }
 
   return (
     <AuthPageContainer>
--- app/routes/(auth)/signup/page.tsx
+++ app/routes/(auth)/signup/page.tsx
@@ -1,5 +1,9 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router';
+
 import SignupPageContent from './_components/SignupPageContent';
 
+import emitSnackbarAlert from '~/routes/_helpers/snackbarAlert';
 import type { Route } from './+types/page';
 
 export function meta(_meta: Route.MetaArgs) {
@@ -9,6 +13,36 @@ export function meta(_meta: Route.MetaArgs) {
   ];
 }
 
+const useSession = (
+  await (import.meta.env.DEV
+    ? import('~/hooks/react-query/auth/__mocks__/useSession')
+    : import('~/hooks/react-query/auth/useSession'))
+).default;
+
 export default function SignupPage() {
+  const session = useSession();
+
+  const sessionData = session.data?.session;
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!session.isPending && sessionData) {
+      const redirect = async () => {
+        const firstName = sessionData.name.split(' ')[0];
+
+        await navigate('/', { replace: true });
+
+        emitSnackbarAlert({
+          alertText: `Welcome back, ${firstName}!`,
+        });
+      };
+
+      redirect();
+    }
+  }, [navigate, session.isPending, sessionData]);
+
+  if (session.isPending || sessionData) return null;
+
   return <SignupPageContent />;
 }
