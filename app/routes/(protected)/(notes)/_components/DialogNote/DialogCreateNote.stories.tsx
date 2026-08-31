6206d97a0221da6d6d56f1dd7d240d5635b72b47
Syahda Romansyah <109145251+codewithsyahda@users.noreply.github.com>
2026-08-31 07:57:25 +0700

    feat: update the Create Note dialog (#84)

--- app/routes/(protected)/(notes)/_components/DialogNote/DialogCreateNote.stories.tsx
+++ app/routes/(protected)/(notes)/_components/DialogNote/DialogCreateNote.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import { Toaster } from 'sonner';
-import { withRouter } from 'storybook-addon-remix-react-router';
-import { expect, fn, waitFor } from 'storybook/test';
+import type { Canvas, Globals } from 'storybook/internal/csf';
+import { expect, fn, waitFor, type UserEventObject } from 'storybook/test';
 
 import reactQueryDecorator from '.storybook/decorators/reactQuery';
 import { mockPostNoteHandler } from '.storybook/parameters/msw/notesHandlers';
@@ -11,7 +11,10 @@ const meta = {
   title: 'Composites/DialogCreateNote',
   component: DialogCreateNoteComponent,
   parameters: {
-    layout: 'centered',
+    layout: 'fullscreen',
+    msw: {
+      handlers: [mockPostNoteHandler()],
+    },
   },
   args: {
     onClose: fn(),
@@ -24,12 +27,6 @@ const meta = {
         <Toaster duration={Infinity} />
       </>
     ),
-    withRouter,
-    (Story) => (
-      <div className="min-w-sm">
-        <Story />
-      </div>
-    ),
   ],
 } satisfies Meta<typeof DialogCreateNoteComponent>;
 
@@ -37,7 +34,10 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const DefaultMobile: Story = {
+  globals: {
+    viewport: { value: 'mobile1', isRotated: false },
+  },
   play: async ({ canvas }) => {
     await waitFor(() => {
       expect(
@@ -47,6 +47,10 @@ export const Default: Story = {
       ).toHaveFocus();
     });
   },
+};
+
+export const Default: Story = {
+  play: DefaultMobile.play,
 };
 
 export const FocusTrap: Story = {
@@ -142,65 +146,68 @@ export const FocusTrap: Story = {
   },
 };
 
-export const Creating: Story = {
+async function playBasicCreateNote({
+  globals,
+  canvas,
+  userEvent,
+}: Readonly<{
+  globals: Globals;
+  canvas: Canvas;
+  userEvent: UserEventObject;
+}>) {
+  await userEvent.type(
+    canvas.getByRole('textbox', { name: 'Title note' }),
+    'Title by Storybook',
+  );
+
+  await userEvent.type(
+    canvas.getByRole('textbox', { name: 'Content note' }),
+    'Content by Storybook',
+  );
+
+  if (globals?.viewport?.value === 'mobile1') {
+    await userEvent.pointer({
+      keys: '[TouchA]',
+      target: canvas.getByRole('button', {
+        name: 'Save',
+      }),
+    });
+  } else {
+    await userEvent.click(
+      canvas.getByRole('button', {
+        name: 'Save',
+      }),
+    );
+  }
+
+  await expect(
+    canvas.getByRole('button', {
+      name: 'Save',
+    }),
+  ).toBeDisabled();
+}
+
+export const CreatingMobile: Story = {
+  globals: DefaultMobile.globals,
   parameters: {
     msw: {
       handlers: [mockPostNoteHandler({ delayInfinite: true })],
     },
   },
-  play: async ({ canvas, userEvent }) => {
-    await userEvent.type(
-      canvas.getByRole('textbox', { name: 'Title note' }),
-      'Title by Storybook',
-    );
-
-    await userEvent.type(
-      canvas.getByRole('textbox', { name: 'Content note' }),
-      'Content by Storybook',
-    );
-
-    await userEvent.click(
-      canvas.getByRole('button', {
-        name: 'Save',
-      }),
-    );
-
-    await expect(
-      canvas.getByRole('button', {
-        name: 'Save',
-      }),
-    ).toBeDisabled();
+  play: async ({ globals, canvas, userEvent }) => {
+    await playBasicCreateNote({ globals, canvas, userEvent });
   },
 };
 
-export const CreateNoteSuccess: Story = {
-  parameters: {
-    msw: {
-      handlers: [mockPostNoteHandler()],
-    },
-  },
-  play: async ({ args, canvas, userEvent }) => {
-    await userEvent.type(
-      canvas.getByRole('textbox', { name: 'Title note' }),
-      'Title by Storybook',
-    );
+export const Creating: Story = {
+  parameters: CreatingMobile.parameters,
+  play: CreatingMobile.play,
+};
 
-    await userEvent.type(
-      canvas.getByRole('textbox', { name: 'Content note' }),
-      'Content by Storybook',
-    );
-
-    await userEvent.click(
-      canvas.getByRole('button', {
-        name: 'Save',
-      }),
-    );
-
-    await expect(
-      canvas.getByRole('button', {
-        name: 'Save',
-      }),
-    ).toBeDisabled();
+export const CreateNoteSuccessMobile: Story = {
+  globals: DefaultMobile.globals,
+  play: async ({ args, globals, canvas, userEvent }) => {
+    await playBasicCreateNote({ globals, canvas, userEvent });
 
     await waitFor(async () => {
       await expect(canvas.getByText('Note created')).toBeVisible();
@@ -208,4 +215,58 @@ export const CreateNoteSuccess: Story = {
 
     await expect(args.onClose).toHaveBeenCalledOnce();
   },
+};
+
+export const CreateNoteSuccess: Story = {
+  play: CreateNoteSuccessMobile.play,
+};
+
+export const CreateNote400ErrorMobile: Story = {
+  globals: DefaultMobile.globals,
+  parameters: {
+    msw: {
+      handlers: [mockPostNoteHandler({ errorStatus: '400' })],
+    },
+  },
+  play: async ({ args, globals, canvas, userEvent }) => {
+    await playBasicCreateNote({ globals, canvas, userEvent });
+
+    await waitFor(async () => {
+      await expect(
+        canvas.getByText('One or more body request fields are invalid.'),
+      ).toBeVisible();
+    });
+
+    await expect(args.onClose).not.toHaveBeenCalledOnce();
+  },
+};
+
+export const CreateNote400Error: Story = {
+  parameters: CreateNote400ErrorMobile.parameters,
+  play: CreateNote400ErrorMobile.play,
+};
+
+export const CreateNote500ErrorMobile: Story = {
+  globals: DefaultMobile.globals,
+  parameters: {
+    msw: {
+      handlers: [mockPostNoteHandler({ errorStatus: '500' })],
+    },
+  },
+  play: async ({ args, globals, canvas, userEvent }) => {
+    await playBasicCreateNote({ globals, canvas, userEvent });
+
+    await waitFor(async () => {
+      await expect(
+        canvas.getByText('Cannot process the request.'),
+      ).toBeVisible();
+    });
+
+    await expect(args.onClose).not.toHaveBeenCalledOnce();
+  },
+};
+
+export const CreateNote500Error: Story = {
+  parameters: CreateNote500ErrorMobile.parameters,
+  play: CreateNote500ErrorMobile.play,
 };
